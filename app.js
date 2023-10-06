6ca90a94d4eafd3bb61ecb63f5800f00f0a62f4c
Yoonjieut <qmdldos@gmail.com>
2023-10-06 14:51:28 +0900

    container 클릭 이벤트 추가

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotList = document.getElementsByClassName('dotTarget');
 console.log(gameArea);
 
 
+// * 3개의 .dotTarget 추가
 for(i=0; i<3; i++){
   let someDot = document.createElement('div')
   gameArea.appendChild(someDot);
@@ -13,3 +14,8 @@ for(i=0; i<3; i++){
 }
 // console.log(someDot);
 console.log(dotList);
+
+// * container 클릭 이벤트
+gameArea.addEventListener('click',function(target){
+  console.log(target);
+});
