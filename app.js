f20705445de11c2b29ab2d6b0e062d4746b1dc25
YoonJieut <qmdldos@gmail.com>
2023-10-09 19:59:38 +0900

    클릭 시 삭제로직이 작동하지 않아서 수정함

--- app.js
+++ app.js
@@ -16,32 +16,8 @@ let numHMax = parseInt(gameArea.clientHeight-20);
 console.dir(gameArea);
 
 
-// * 초기 3개 세팅
-for(i=0; i<3; i++){
-  addDot();
-}
-// * .dotTarget 랜덤 +추가 로직
-function addDot(){
-  let someDot = document.createElement('div')
-  gameArea.appendChild(someDot);
-  someDot.className = "dotTarget";
-  someDot.style.left = `${ramdomNumber(0, numWMax)}px`;
-  someDot.style.top = `${ramdomNumber(0, numHMax)}px`;
-}
-// console.log(dotList);
 
-
-// * container 클릭 이벤트
-// 클릭하면 삭제되는 로직
-gameArea.addEventListener('click',function(eventTarget){
-  console.log(eventTarget);
-  if(eventTarget.target === "div.dotTarget"){
-    eventTarget.target.remove();
-    
-    // ? 랜덤 생성 모듈 들어갈 자리
-    addDot()
-  }
-});
+//**** * 함수 선언 파트----------------------------------------
 
 // * 랜덤 모듈 --------------------------
 // 랜덤 함수
@@ -61,6 +37,47 @@ function ramdomNumber(min, max) {
 // gameArea.clientLeft - 좌표값
 // gameArea.clientTop - 좌표값
 
+
+// * .dotTarget 랜덤 +추가 로직
+function addDot(){
+  let someDot = document.createElement('div')
+  gameArea.appendChild(someDot);
+  someDot.className = "dotTarget";
+  someDot.style.left = `${ramdomNumber(0, numWMax)}px`;
+  someDot.style.top = `${ramdomNumber(0, numHMax)}px`;
+}
+// console.log(dotList);
+
+
+
+
+
+
+//* 로직 파트 및 이벤트 리스터 파트 ---------------------------------
+
+
+// * 초기 3개 세팅
+for(i=0; i<3; i++){
+  addDot();
+}
+
+// * container 클릭 이벤트
+// 클릭하면 삭제되는 로직
+gameArea.addEventListener('click',function(eventTarget){
+  // console.log(eventTarget);
+  console.log(eventTarget.target.className);
+  if(eventTarget.target.className ==="dotTarget"){
+    eventTarget.target.remove();
+    
+    // ? 랜덤 생성 모듈 들어갈 자리
+    addDot()
+  } else {
+    console.log('잘못 누르셨습니다.')
+  }
+});
+// ! 껏다 키자 삭제 로직이 작동하지 안하, if문의 조건식을 eventTarget.target === "div.dotTarget"에서 eventTarget.target.className ==="dotTarget" 으로 수정함
+
+
 // * 사라지면 score 로직 추가
 // const scoreList =[];
 
@@ -73,16 +90,22 @@ function ramdomNumber(min, max) {
 
 // * 입력값 setTimeout 시간 세팅
 
+
 // * clear 함수
 // gameArea 안의 모든 것을 지웁니다.
 start.addEventListener('click',function(){
-
+  console.log("start 버튼 누름");
+  removeAll();
 });
 
 function removeAll(){
+  console.log('removeAll 시작');
   for(i=0;i<gameArea.children.length; i++){
-    gameArea.removeChild()
+    if(gameArea.lastChild === "div.dotTarget"){
+      gameArea.removeChild(dotList[i]);
+    }
   }
+  console.log('removeAll 종료');
 };
 
 
